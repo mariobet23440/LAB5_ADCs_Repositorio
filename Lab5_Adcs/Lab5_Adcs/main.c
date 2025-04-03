70287aebf7e2bf70cc9b908d2d201468f987a12b
mariobet23440 <bet23440@uvg.edu.gt>
2025-04-03 14:06:52 -0600

    Creación de Archivo

---
+++ Lab5_Adcs/Lab5_Adcs/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab5_Adcs.c
+ *
+ * Created: 3/04/2025 14:05:25
+ * Author : mario
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
